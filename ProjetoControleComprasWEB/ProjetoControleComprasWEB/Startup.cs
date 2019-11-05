a095736a008b333ae741a6756e003c93e9310e89
PatrickSLuz <patricksantos20101@gmail.com>
2019-11-05 10:44:15 -0300

    Ajustes para a Criacao do Banco de Dados e ajuste no _Layout

--- ProjetoControleComprasWEB/ProjetoControleComprasWEB/Startup.cs
+++ ProjetoControleComprasWEB/ProjetoControleComprasWEB/Startup.cs
@@ -60,7 +60,7 @@ namespace ProjetoControleComprasWEB
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
--- ProjetoControleComprasWEB/Repository/Context.cs
+++ ProjetoControleComprasWEB/Repository/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,5 +12,12 @@ namespace Repository
         public Context(DbContextOptions<Context> options) : base(options) { }
 
         // Definir as Classes que irão se tornar tabelas no BD
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Setor> Setores { get; set; }
+        public DbSet<Agente> Agentes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Orcamento> Orcamentos { get; set; }
+        public DbSet<ItemPedido> ItemPedido { get; set; }
     }
 }

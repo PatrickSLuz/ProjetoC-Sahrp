867ce3907ba85d02f484713a6e33064ab5de5f46
Mauricio Kreling <mauriciokreling10@gmail.com>
2019-11-28 09:45:39 -0300

    Criação ProdutoDAO

--- ProjetoControleComprasWEB/ProjetoControleComprasWEB/Controllers/ProdutoController.cs
+++ ProjetoControleComprasWEB/ProjetoControleComprasWEB/Controllers/ProdutoController.cs
@@ -9,32 +9,10 @@ using System.Threading.Tasks;
 
 namespace ProjetoControleComprasWEB.Controllers
 {
+
+  
     public class ProdutoController : Controller
     {
-        public bool CadastrarProduto(Produto produto)
-        {
-            //Editar o que ja esta cadastrado
-            if (BuscarProdutoPorNome(produto.ProdutoId) == null)
-            {
-               return _Context
-            }
-        }
-
-        public BuscarProdutoPorID(int id)
-        {
-            return 
-        }
-
-        public Produto BuscarProdutoPorNome(Produto produto)
-        {
-            return 
-        }
-        public static List<Produto> ListarProdutos()
-        {
-
-        }
-            {
-                
-            }
+        
     }
 }
---
+++ ProjetoControleComprasWEB/Repository/ProdutoDAO.cs
@@ -0,0 +1,59 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    class ProdutoDAO : IRepository<Produto>
+    {
+        private readonly Context _context;
+
+        public ProdutoDAO(Context context)
+        {
+            _context = context;
+        }
+
+        public bool Cadastrar(Produto objeto)
+        {
+            if(BuscarPorId(objeto.ProdutoId)!=null)
+            {
+                _context.Entry(objeto).CurrentValues.SetValues(objeto);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                if(BuscarProdutoPorNome(objeto)!=null)
+                {
+                    _context.Produtos.Add(objeto);
+                    _context.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public Produto BuscarProdutoPorNome(Produto produto)
+        {
+            return _context.Produtos.FirstOrDefault(x => x.NomeProduto.Equals(produto.NomeProduto));
+        }
+
+        public Produto BuscarPorId(int id)
+        {
+            return _context.Produtos.Find(id);
+        }
+
+        public List<Produto> ListarTodos()
+        {
+            return _context.Produtos.ToList();
+        }
+
+ 
+    }
+}
